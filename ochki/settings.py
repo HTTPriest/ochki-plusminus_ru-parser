b729d5169cc6a74b96f770cf2d979d640abd449c
datapriest <htpriestx@gmail.com>
2018-04-09 15:00:41 +0300

    login; render.json

--- ochki/settings.py
+++ ochki/settings.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Scrapy settings for ochki project
+# Scrapy settings for litres project
 #
 # For simplicity, this file contains only settings considered important or
 # commonly used. You can find more settings consulting the documentation:
@@ -9,25 +9,25 @@
 #     https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://doc.scrapy.org/en/latest/topics/spider-middleware.html
 
-BOT_NAME = 'ochki'
+BOT_NAME = 'basic'
 
 SPIDER_MODULES = ['ochki.spiders']
 NEWSPIDER_MODULE = 'ochki.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'ochki (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64; rv:59.0) Gecko/20100101 Firefox/59.0'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 6
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -47,13 +47,13 @@ ROBOTSTXT_OBEY = True
 # Enable or disable spider middlewares
 # See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
 #SPIDER_MIDDLEWARES = {
-#    'ochki.middlewares.OchkiSpiderMiddleware': 543,
+#    'litres.middlewares.LitresSpiderMiddleware': 543,
 #}
 
 # Enable or disable downloader middlewares
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
 #DOWNLOADER_MIDDLEWARES = {
-#    'ochki.middlewares.OchkiDownloaderMiddleware': 543,
+#    'litres.middlewares.LitresDownloaderMiddleware': 543,
 #}
 
 # Enable or disable extensions
@@ -65,7 +65,7 @@ ROBOTSTXT_OBEY = True
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 #ITEM_PIPELINES = {
-#    'ochki.pipelines.OchkiPipeline': 300,
+#    'litres.pipelines.LitresPipeline': 300,
 #}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
@@ -88,3 +88,29 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+
+# Enable and configure HTTP caching (disabled by default)
+# See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+HTTPCACHE_ENABLED = False
+#HTTPCACHE_EXPIRATION_SECS = 0
+#HTTPCACHE_DIR = 'httpcache'
+#HTTPCACHE_IGNORE_HTTP_CODES = []
+#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+
+SPLASH_URL = 'http://localhost:8050'
+
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy_splash.SplashCookiesMiddleware': 723,
+    'scrapy_splash.SplashMiddleware': 725,
+    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+}
+
+DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
+
+#HTTPCACHE_STORAGE = 'scrapy_splash.SplashAwareFSCacheStorage'
+
+#ROTATING_PROXY_LIST_PATH = 'proxy.txt'
+
+
--- ochki/spiders/basic.py
+++ ochki/spiders/basic.py
@@ -1,11 +1,64 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import base64
+from scrapy_splash import SplashRequest, SplashFormRequest
+from time import time
 
 
 class BasicSpider(scrapy.Spider):
     name = 'basic'
     allowed_domains = ['ochki-plusminus.ru']
-    start_urls = ['http://ochki-plusminus.ru/']
+
+
+    def screenshot(self, response):
+        imgdata = base64.b64decode(response.data['png'])
+        filename = 'screenshot%s.png' % time()
+        with open(filename, 'wb') as f:
+            f.write(imgdata)
+
+
+    def start_requests(self):
+        body = '''-----------------------------5520711821930979191957072406
+Content-Disposition: form-data; name="email"
+
+htpriestx@gmail.com
+-----------------------------5520711821930979191957072406
+Content-Disposition: form-data; name="password"
+
+all5885
+-----------------------------5520711821930979191957072406--
+
+        '''
+        yield SplashFormRequest('http://ochki-plusminus.ru/index.php?route=account/login',
+                            callback=self.login,
+                            endpoint='render.json',
+                            args={'wait': 2,
+                                  'html': 1,
+                                  'png': 1},
+                            formdata={'email': 'htpriestx@gmail.com',
+                                      'password': 'all5885'}
+                            )
+
+
+    def login(self, response):
+        logout = response.xpath('//a[@href=contains(., "logout")]')
+        self.log('Page loaded')
+        self.log(logout)
+        self.screenshot(response)
+
+        yield SplashRequest('http://ochki-plusminus.ru/',
+                                endpoint='render.json',
+                                args={'wait': 2,
+                                      'html': 1,
+                                      'png': 1})
+
 
     def parse(self, response):
-        pass
+        self.screenshot(response)
+        registration = response.xpath('//a[@target="регистрации"]')
+        if registration:
+            self.log('LOGIN HAS FAILED')
+
+
+
+
