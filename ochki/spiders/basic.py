dabbf36955ce17231ae5131ed2e23974de0c67bb
datapriest <htpriestx@gmail.com>
2018-04-09 16:33:08 +0300

    execute endpoint: succesful log in

--- ochki/spiders/basic.py
+++ ochki/spiders/basic.py
@@ -12,45 +12,64 @@ class BasicSpider(scrapy.Spider):
 
     def screenshot(self, response):
         imgdata = base64.b64decode(response.data['png'])
-        filename = 'screenshot%s.png' % time()
+        filename = 'save/screenshot%s.png' % time()
         with open(filename, 'wb') as f:
             f.write(imgdata)
 
+    lua_script = '''
+            function main(splash)
+                if splash.args.cookies then
+                    splash:init_cookies(splash.args.cookies)
+                end
+                assert(splash:go{
+                    splash.args.url,
+                    headers=splash.args.headers,
+                    http_method=splash.args.http_method,
+                    body=splash.args.body,
+                    formdata=splash.args.formdata
+                })
+                assert(splash:wait(0.5))
+
+                local entries = splash:history()
+                local last_response = entries[#entries].response
+
+                return {
+                    url = splash:url(),
+                    headers = last_response.headers,
+                    http_status = last_response.status,
+                    cookies = splash:get_cookies(),
+                    html = splash:html(),
+                    png = splash:png(),
+                }
+            end
+            '''
 
     def start_requests(self):
-        body = '''-----------------------------5520711821930979191957072406
-Content-Disposition: form-data; name="email"
-
-htpriestx@gmail.com
------------------------------5520711821930979191957072406
-Content-Disposition: form-data; name="password"
-
-all5885
------------------------------5520711821930979191957072406--
-
-        '''
-        yield SplashFormRequest('http://ochki-plusminus.ru/index.php?route=account/login',
+        yield SplashRequest('http://ochki-plusminus.ru/index.php?route=account/login',
                             callback=self.login,
-                            endpoint='render.json',
+                            endpoint='execute',
+                            method='POST',
                             args={'wait': 2,
-                                  'html': 1,
-                                  'png': 1},
-                            formdata={'email': 'htpriestx@gmail.com',
-                                      'password': 'all5885'}
+                                  'lua_source': self.lua_script,
+                                  'formdata': {
+                                      'email': 'htpriestx@gmail.com',
+                                      'password': 'all5885'
+                                              },
+                                  }
                             )
-
 
     def login(self, response):
         logout = response.xpath('//a[@href=contains(., "logout")]')
         self.log('Page loaded')
-        self.log(logout)
+        #self.log(logout)
         self.screenshot(response)
 
         yield SplashRequest('http://ochki-plusminus.ru/',
-                                endpoint='render.json',
-                                args={'wait': 2,
-                                      'html': 1,
-                                      'png': 1})
+                            endpoint='execute',
+                            method='GET',
+                            args={'wait': 2,
+                                  'lua_source': self.lua_script,
+                                  })
 
 
     def parse(self, response):
@@ -58,6 +77,8 @@ all5885
         registration = response.xpath('//a[@target="регистрации"]')
         if registration:
             self.log('LOGIN HAS FAILED')
+        else:
+            self.log('LOGIN SUCCESSFUL')
 
 
 
